182b20038d726bb4c9ea74a9cc815f5bf94b504f
dhirajapp <dhirajkumar31298@gmail.com>
2023-09-15 20:14:01 +0530

    launch a browser and hit a url automation exercise then validate the header

--- cypress/e2e/MyFirstTest.cy.js
+++ cypress/e2e/MyFirstTest.cy.js
@@ -1,5 +1,12 @@
 describe('My First Test', () => {
-  it('test1', () => {
+  it('positive scenario', () => {
     cy.visit('https://automationexercise.com/')
+    cy.title().should('eq','Automation Exercise')
   })
+
+  it('Negative scenario', () => {
+    cy.visit('https://automationexercise.com/')
+    cy.title().should('eq','Manual Exercise')
+  })
+
 })
