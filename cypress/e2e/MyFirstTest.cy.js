e65acabbe12a0ec849a01410fb483cc762a90dcc
dhirajapp <dhirajkumar31298@gmail.com>
2023-09-18 22:06:07 +0530

    write a automation script for produt page

--- cypress/e2e/MyFirstTest.cy.js
+++ cypress/e2e/MyFirstTest.cy.js
@@ -6,9 +6,19 @@ describe('My First Test', () => {
 
   it('Home page', () => {
     cy.visit('https://automationexercise.com/')
-    cy.get("i.fa fa-home").click()
     cy.title().should('eq','Automation Exercise')
-    cy.get(".col-sm-6").contains("Automation")
+    cy.get("header[id='header'] li:nth-child(1) a:nth-child(1)").click()
+    cy.get("div[class='item active'] span:nth-child(1)").contains("Automation")
+  })
+
+
+  it('Product page', () => {
+    cy.visit('https://automationexercise.com/')
+    cy.title().should('eq','Automation Exercise')
+    cy.get("a[href='/products']").click()
+    cy.get("#search_product").type("T-shirt")
+    cy.get("#submit_search").click()
+    cy.get("body > section:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > p:nth-child(2)").contains("Pure Cotton V-Neck T-Shirt")
   })
 
 })
