f4ac7b0353c72e98dd2551f4af73fcd21922160b
dhirajapp <dhirajkumar31298@gmail.com>
2023-09-20 21:59:23 +0530

    product add to cart test automated

--- cypress/e2e/AutomationExercise/produt.cy.js
+++ cypress/e2e/AutomationExercise/produt.cy.js
@@ -18,7 +18,18 @@ describe('My First Test', () => {
     cy.get("a[href='/products']").click()
     cy.get("#search_product").type("T-shirt")
     cy.get("#submit_search").click()
-    cy.get("body > section:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > p:nth-child(2)").contains("Pure Cotton V-Neck T-Shirt")
+    cy.get("body > section:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > p:nth-child(2)")
+      .contains("Pure Cotton V-Neck T-Shirt")
+    cy.get("a[href='/product_details/28']").click()
+    cy.title().should('eq','Automation Exercise - Product Details')
+    cy.get("button[type='button']").click()
+    cy.get("div[id='cartModal'] p:nth-child(1)").contains("Your product has been added to cart.")
+    cy.get("body > section:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > p:nth-child(2) > a:nth-child(1) > u:nth-child(1)")
+     .click()
+     cy.title().should('eq','Automation Exercise - Checkout')
+
+  
+  
   })
 
 })
