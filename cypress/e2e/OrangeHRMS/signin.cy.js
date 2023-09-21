5b1bb758f0bed7f24905564384a1ceba746693c2
dhirajapp <dhirajkumar31298@gmail.com>
2023-09-21 22:11:14 +0530

    implement xpath and implicit assertions

--- cypress/e2e/OrangeHRMS/signin.cy.js
+++ cypress/e2e/OrangeHRMS/signin.cy.js
@@ -1,7 +1,17 @@
 describe('Login', () => {
     it('Login page', () => {
       cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+      cy.url().should('include','orangehrmlive')
+        .and('eq','https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        .and('contain','orangehrm')
       cy.title().should('eq','OrangeHRM')
+      cy.xpath("//input[@placeholder='Username']").type('Admin')
+      cy.xpath("//input[@placeholder='Username']").should('have.value','Admin')
+      cy.xpath("//input[@placeholder='Password']").type('admin123')
+      cy.xpath("//input[@placeholder='Password']").should('have.value','admin123')
+      cy.xpath("//button[normalize-space()='Login']").click()
+      cy.url().should('eq','https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
+      
       
     })
 })
